3f1142f7d7c31265b25ad3a27df7c48c376381d3
leasek-lisd <143102157+leasek-lisd@users.noreply.github.com>
2023-11-24 10:26:50 -0600

    Create Wheels.java

---
+++ base code/org/firstinspires/ftc/teamcode/Wheels.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+
+abstract class Wheels {
+  public abstract void turnFrontLeft(double power);
+  public abstract void turnFrontRight(double power);
+  public abstract void turnBackLeft(double power);
+  public abstract void turnBackRight(double power);
+    // todo: write your code here
+}
