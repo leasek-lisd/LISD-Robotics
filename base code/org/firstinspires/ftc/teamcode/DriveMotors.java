ad13d5a17aec3d506ceb6315f64199251d78ab45
leasek-lisd <143102157+leasek-lisd@users.noreply.github.com>
2023-11-26 20:28:55 -0600

    Update DriveMotors.java

--- base code/org/firstinspires/ftc/teamcode/DriveMotors.java
+++ base code/org/firstinspires/ftc/teamcode/DriveMotors.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutionException;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-public class DriveMotors extends Wheels  {
+public class DriveMotors   {
     
     private DcMotor frontLeftDrive;
     private DcMotor frontRightDrive;
@@ -27,46 +27,38 @@ public class DriveMotors extends Wheels  {
     
     }
 
-// The next four methods are where you setup your wheels
-// add code that makes your wheel roll forward with a positive value for power
-// "Forward" meaning that the top of the wheel rolls toward the front of the bot
-
-  public void turnFrontLeft(double power){
-    frontLeftDrive.setPower(power);
+  public boolean areBusy(){
+    return((
+      frontLeftDrive.isBusy()
+      ||frontRightDrive.isBusy()
+      ||backRightDrive.isBusy()
+      ||backLeftDrive.isBusy()
+      ));
   }
-  
-  public void turnFrontRight(double power){
-    frontRightDrive.setPower(power);
-  }
-  
-  public void turnBackLeft(double power){
-    backLeftDrive.setPower(power);
-  }
-  
-  public void turnBackRight(double power){
-    backRightDrive.setPower(power);
-  }
-  
-  
-  
   public void slideFront(double x){
     double powerSetting = .35;
-    turnFrontLeft(x*powerSetting);
-    turnFrontRight(-x*powerSetting);
+    backLeftDrive.setPower(x*powerSetting);
+    backRightDrive.setPower(-x*powerSetting);
   }
   
   public void slideRear(double x){
     double powerSetting = .55;
-    turnBackLeft(x*powerSetting);
-    turnBackRight(-x*powerSetting);
+    frontLeftDrive.setPower(x*powerSetting);
+    frontRightDrive.setPower(-x*powerSetting);
   }
   
   //public void slideRightTimed(double x)
-  
+  public void move2(double x, double y, double turn,double offset){
+    move3(x,y,turn,offset);
+  }
   public void move(double x, double y, double turn){
-    
+    move3(x,y,turn,0);
+  }
+  public void move3(double x, double y, double turn,double offset){
+    encoderModeOff();
         double rawTheta = Math.atan2(y,x);
-        double theta = rawTheta -Math.PI/4;
+        double theta = rawTheta -Math.PI/4+2*offset;
+        
         double leftFront = 0;
         double rightFront = 0;
         double leftRear = 0;
@@ -88,17 +80,17 @@ public class DriveMotors extends Wheels  {
           leftRear    /= power + Math.abs(turn);
           rightRear  /= power + Math.abs(turn);
         }
-        turnFrontLeft(leftFront);
-        turnFrontRight(rightFront);
-        turnBackLeft(leftRear);
-        turnBackRight(rightRear);
+        frontLeftDrive.setPower(leftFront);
+        frontRightDrive.setPower(rightFront);
+        backLeftDrive.setPower(leftRear);
+        backRightDrive.setPower(rightRear);
     }
     
   public void allStop(){
-    turnFrontLeft(0);
-    turnFrontRight(0);
-    turnBackLeft(0);
-    turnBackRight(0);
+    frontLeftDrive.setPower(0);
+    frontRightDrive.setPower(0);
+    backLeftDrive.setPower(0);
+    backRightDrive.setPower(0);
   }
   
     public void forward(double speed){
@@ -116,6 +108,133 @@ public class DriveMotors extends Wheels  {
     public void strafeRight(double speed){
       move(speed,0,.04);    
     }
+    
+    public void forwardInches(double inches){
+      //int steps = (int) (inches * 36.6735);
+      int steps = (int) (inches * 44);
+      forwardSteps(steps,.5);
+    }
+    public void backInches(double inches){
+      //int steps = (int) (inches * 36.6735);
+      int steps = (int) (inches * 44);
+      forwardSteps(-steps,.5);
+      while(frontLeftDrive.isBusy()){};
+    }
+    
+    public void forwardSteps(int motorSteps, double speed){
+     /*
+      frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      frontLeftDrive.setTargetPosition(-motorSteps);
+      frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      frontLeftDrive.setPower(speed);
+      
+      
+      frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      frontRightDrive.setTargetPosition(-motorSteps);
+      frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      frontRightDrive.setPower(speed);
+      
+      
+      backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backLeftDrive.setTargetPosition(-motorSteps);
+      backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      backLeftDrive.setPower(speed);
+      
+      
+      backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backRightDrive.setTargetPosition(-motorSteps);
+      backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      backRightDrive.setPower(speed);
+      */
+      resetAllEncoders();
+      setTargetTo(-motorSteps);
+      setRunToPosition();
+      setAllPower(speed);
+      // encoderModeOn();
+      
+     // while(frontLeftDrive.isBusy()){}
+     // allStop();
+     // while(opModeIsActive&&)
+    }
+    
+    
+    
+    public void setRunToPosition(){
+      frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    
+    public void resetAllEncoders(){
+      frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    
+    public void setTargetTo(int stepPosition){
+      frontLeftDrive.setTargetPosition(stepPosition);
+      frontRightDrive.setTargetPosition(stepPosition);
+      backLeftDrive.setTargetPosition(stepPosition);
+      backRightDrive.setTargetPosition(stepPosition);
+    }
+    
+    public void setAllPower(double speed){
+      frontLeftDrive.setPower(speed);
+      frontRightDrive.setPower(speed);
+      backLeftDrive.setPower(speed);
+      backRightDrive.setPower(speed);
+    }
+    
+    public void encoderModeOff(){
+      frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    
+    public void leftInches(double inches){
+      //int steps = (int) (inches * 36.6735);
+      int steps = (int) (inches * 44);
+      leftSteps(-steps,.5);
+    }
+    
+    public void rightInches(double inches){
+      //int steps = (int) (inches * 36.6735);
+      int steps = (int) (inches * 44);
+      leftSteps(steps,.5);
+    }
+    public void leftSteps(int motorSteps, double speed){
+      frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      frontLeftDrive.setTargetPosition(-motorSteps);
+      frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      frontLeftDrive.setPower(speed);
+      frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      
+      
+      frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      frontRightDrive.setTargetPosition(motorSteps);
+      frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      frontRightDrive.setPower(speed);
+      frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      
+      backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backLeftDrive.setTargetPosition(motorSteps);
+      backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      backLeftDrive.setPower(speed);
+      backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      
+      backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      backRightDrive.setTargetPosition(-motorSteps);
+      backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      backRightDrive.setPower(speed);
+      backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      
+     // while(frontLeftDrive.isBusy()){}
+     // allStop();
+     // while(opModeIsActive&&)
+    }    
     
     public void forwardTimed(double speed,int time) 
     throws InterruptedException, ExecutionException
